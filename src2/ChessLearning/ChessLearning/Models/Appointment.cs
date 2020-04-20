86c7f20fdb6ec369a50d6ce1744233fbc6917765
colin <gaoshangguo@gmail.com>
2020-04-21 00:31:08 +0800

    1

--- src2/ChessLearning/ChessLearning/Models/Appointment.cs
+++ src2/ChessLearning/ChessLearning/Models/Appointment.cs
@@ -18,29 +18,49 @@ namespace ChessLearning.Models
         [Display(Name = "预约完成")]
         Done,
     }
+
+    public enum StudentColorEnum
+    {
+        [Display(Name = "白色")]
+        White,
+        [Display(Name = "黑色")]
+        Black
+    }
     public class Appointment : BasePoco
     {
 
         public string StudentId { get; set; }
+        
         [Display(Name = "学生昵称")]
         public string StudentNickName { get; set; }
 
+        [Display(Name = "学生链接")]
+        public string StudentUrl { get; set; }
+        
         public string TeacherId { get; set; }
+        
         [Display(Name = "老师昵称")]
         public string TeacherNickName { get; set; }
-
+        [Display(Name = "老师链接")]
+        public string TeacherUrl { get; set; }
         [Display(Name = "预约状态")]
         public AppointmentStatusEnum Status { get; set; }
 
         [Display(Name = "预约发生时间")]
         public string DoingTime { get; set; }
+        
         [Display(Name = "预约标识")]
         public string RoomCode { get; set; }
+        
+        [Display(Name = "学员选色")]
+        public StudentColorEnum StudentColor { get; set; }
 
         [Display(Name = "预约创建时间")]
         public string CreatedTime { get; set; }
+        
         [Display(Name = "预约创建人")]
         public string CreatedUserId { get; set; }
+        
         [Display(Name = "备注")]
         public string Remark { get; set; }
     }
